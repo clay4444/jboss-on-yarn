44965c89140dc0c16d326e81f7b06b56216eee62
loushang <loushang@vipkid.com.cn>
2019-09-27 20:39:40 +0800

    update

--- src/main/java/org/yarnbook/JBossApplicationMaster.java
+++ src/main/java/org/yarnbook/JBossApplicationMaster.java
@@ -85,7 +85,7 @@ public class JBossApplicationMaster {
     @SuppressWarnings("rawtypes")
     private AMRMClientAsync resourceManager;  //负责和ResourceManager进行通信，以此来提供异步更新事件，比如容器分配和完成，内部通过一个线程周期性的和RM进行通信；
     private NMClientAsync nmClientAsync;  //负责和NodeManager进行通信，得到响应，以此来提供异步更新事件，内部包含一个线程池，每个线程通过使用${NMClientImpl}与每个独立的NodeManager进行通信，线程池大小可配置；
-    private NMCallbackHandler containerListener;
+    private NMCallbackHandler containerListener; //NodeManager的回调
 
     private ApplicationAttemptId appAttemptID;
 
@@ -95,7 +95,7 @@ public class JBossApplicationMaster {
 
     private int numTotalContainers = 2;  //container 个数
     private int containerMemory = 1024;  //单个container的内存大小
-    private int requestPriority;    //优先级
+    private int requestPriority;       //优先级
 
     private String adminUser;           //adminUser
     private String adminPassword;      //adminPassword
@@ -114,7 +114,7 @@ public class JBossApplicationMaster {
     private volatile boolean done;
     private volatile boolean success;
 
-    private List<Thread> launchThreads = new ArrayList<Thread>();  //多个线程？
+    private List<Thread> launchThreads = new ArrayList<Thread>();  //多个线程，每个线程负责启动一个container
 
     /**
      * @param args
@@ -129,7 +129,7 @@ public class JBossApplicationMaster {
             if (!doRun) {
                 System.exit(0);
             }
-            result = appMaster.run();  //run方法
+            result = appMaster.run();  //run方法，主要的执行方法；
         } catch (Throwable t) {
             LOG.log(Level.SEVERE, "Error running JBossApplicationMaster", t);
             System.exit(1);
@@ -309,21 +309,29 @@ public class JBossApplicationMaster {
     public boolean run() throws YarnException, IOException {
         LOG.info("Starting JBossApplicationMaster");
 
+        /**
+         * 构建 ResourceManager 通信客户端，包含一个 rm 事件(container分配/container完成)回调器
+         */
         AMRMClientAsync.CallbackHandler allocListener = new RMCallbackHandler();
         resourceManager = AMRMClientAsync.createAMRMClientAsync(1000,
                 allocListener);
         resourceManager.init(conf);
         resourceManager.start();
 
+        /**
+         * 构建 NodeManager 通信客户端，包含一个 nm 事件(主要是container的状态变更)回调器
+         */
         containerListener = new NMCallbackHandler();
         nmClientAsync = new NMClientAsyncImpl(containerListener);
         nmClientAsync.init(conf);
         nmClientAsync.start();
 
+        //使用构建好的ResourceManager客户端注册ApplicationMaster，成功后启动和ResourceManager的通信线程
         RegisterApplicationMasterResponse response = resourceManager
                 .registerApplicationMaster(appMasterHostname, appMasterRpcPort,
                         appMasterTrackingUrl);
 
+        //查看最大可用内存
         int maxMem = response.getMaximumResourceCapability().getMemory();
         LOG.info("Max mem capabililty of resources in this cluster " + maxMem);
 
@@ -337,12 +345,23 @@ public class JBossApplicationMaster {
             containerMemory = maxMem;
         }
 
+        /**
+         * 按照设置的numTotalContainers，请求container，然后触发ResourceManager的回调；
+         * 当收到分配的container的回调事件时，使用线程池构建CLC，启动container，
+         */
         for (int i = 0; i < numTotalContainers; ++i) {
             ContainerRequest containerAsk = setupContainerAskForRM();
             resourceManager.addContainerRequest(containerAsk);
         }
-        numRequestedContainers.set(numTotalContainers);
+        numRequestedContainers.set(numTotalContainers); //要请求的container一开始 = 指定的container总数
 
+        /**
+         * 循环等待结束，最终的状态可能是：
+         * 0.所有请求的container都成功结束 (最好的情况，也应该是大部分的时候出现的情况)
+         * 1.所有请求的container都已经结束，（ 不保证所有container都正常结束，可能有的container是无法恢复的失败 ）
+         * 2.rm 因为不同步或者其他原因，希望 shutdown 此ApplicationMaster
+         * 2.ApplicationMaster 发生异常 / 和rm 通信异常
+         */
         while (!done) {
             try {
                 Thread.sleep(200);
@@ -357,7 +376,7 @@ public class JBossApplicationMaster {
     private void finish() {
         for (Thread launchThread : launchThreads) {
             try {
-                launchThread.join(10000);
+                launchThread.join(10000);       //等待所有启动container的线程执行完毕；
             } catch (InterruptedException e) {
                 LOG.info("Exception thrown in thread join: " + e.getMessage());
                 e.printStackTrace();
@@ -365,27 +384,26 @@ public class JBossApplicationMaster {
         }
 
         LOG.info("Application completed. Stopping running containers");
-        nmClientAsync.stop();
+        nmClientAsync.stop(); //停止NodeManager 客户端
 
         LOG.info("Application completed. Signalling finish to RM");
 
-        FinalApplicationStatus appStatus;
+        FinalApplicationStatus appStatus;  //AppMaster最终状态
         String appMessage = null;
         success = true;
         if (numFailedContainers.get() == 0
                 && numCompletedContainers.get() == numTotalContainers) {
-            appStatus = FinalApplicationStatus.SUCCEEDED;
+            appStatus = FinalApplicationStatus.SUCCEEDED; //此App成功
         } else {
-            appStatus = FinalApplicationStatus.FAILED;
-            appMessage = "Diagnostics." + ", total=" + numTotalContainers
+            appStatus = FinalApplicationStatus.FAILED;     //此App失败
+            appMessage = "Diagnostics." + ", total=" + numTotalContainers //打印诊断信息；
                     + ", completed=" + numCompletedContainers.get()
                     + ", allocated=" + numAllocatedContainers.get()
                     + ", failed=" + numFailedContainers.get();
-            success = false;
+            success = false; //标志位
         }
         try {
-            resourceManager.unregisterApplicationMaster(appStatus, appMessage,
-                    null);
+            resourceManager.unregisterApplicationMaster(appStatus, appMessage,null);//从 RM 取消此 AppMaster
         } catch (YarnException ex) {
             LOG.log(Level.SEVERE, "Failed to unregister application", ex);
         } catch (IOException e) {
@@ -393,10 +411,17 @@ public class JBossApplicationMaster {
         }
 
         done = true;
-        resourceManager.stop();
+        resourceManager.stop(); //停止 ResourceManager 客户端
     }
 
+    //和 ResourceManager 通信的回调
     private class RMCallbackHandler implements AMRMClientAsync.CallbackHandler {
+
+        /**
+         * 当ResourceManager回复一个心跳内容，且心跳内容包含完成的containers的信息时，被调用；
+         * 如果当前心跳既包含完成的container信息，又包含分配的container信息时，此方法在 onContainersAllocated() 方法之前调用；
+         * @param completedContainers
+         */
         @SuppressWarnings("unchecked")
         public void onContainersCompleted(
                 List<ContainerStatus> completedContainers) {
@@ -405,61 +430,65 @@ public class JBossApplicationMaster {
             for (ContainerStatus containerStatus : completedContainers) {
                 LOG.info("Got container status for containerID="
                         + containerStatus.getContainerId() + ", state="
-                        + containerStatus.getState() + ", exitStatus="
-                        + containerStatus.getExitStatus() + ", diagnostics="
+                        + containerStatus.getState() + ", exitStatus="          //退出状态
+                        + containerStatus.getExitStatus() + ", diagnostics="   //诊断信息
                         + containerStatus.getDiagnostics());
 
-                assert (containerStatus.getState() == ContainerState.COMPLETE);
+                assert (containerStatus.getState() == ContainerState.COMPLETE); // ?
 
-                int exitStatus = containerStatus.getExitStatus();
-                if (0 != exitStatus) {
-                    if (ContainerExitStatus.ABORTED != exitStatus) {
-                        numCompletedContainers.incrementAndGet();
-                        numFailedContainers.incrementAndGet();
-                    } else {
-                        numAllocatedContainers.decrementAndGet();
-                        numRequestedContainers.decrementAndGet();
+                int exitStatus = containerStatus.getExitStatus(); //退出码
+                if (0 != exitStatus) {  //非正常结束
+                    if (ContainerExitStatus.ABORTED != exitStatus) {  //真正的失败，不用继续考虑恢复
+                        numCompletedContainers.incrementAndGet(); //已完成+1
+                        numFailedContainers.incrementAndGet();    //失败的+1
+                    } else {                               //container被框架kill了  ABORTED状态可能是被主动释放的 / 节点故障，这种需要继续申请container来恢复
+                        numAllocatedContainers.decrementAndGet(); //已分配的-1，需要重新分配
+                        numRequestedContainers.decrementAndGet(); //已请求的-1，需要重新请求
                     }
-                } else {
-                    numCompletedContainers.incrementAndGet();
+                } else {  //程序正常结束，
+                    numCompletedContainers.incrementAndGet(); //已完成的containers容器+1
                     LOG.info("Container completed successfully."
                             + ", containerId="
                             + containerStatus.getContainerId());
                 }
             }
 
-            int askCount = numTotalContainers - numRequestedContainers.get();
+            int askCount = numTotalContainers - numRequestedContainers.get();  //总共需要的 - 已经请求的 = 还要再请求的
             numRequestedContainers.addAndGet(askCount);
 
             if (askCount > 0) {
                 for (int i = 0; i < askCount; ++i) {
                     ContainerRequest containerAsk = setupContainerAskForRM();
-                    resourceManager.addContainerRequest(containerAsk);
+                    resourceManager.addContainerRequest(containerAsk);      //继续请求
                 }
             }
 
-            if (numCompletedContainers.get() == numTotalContainers) {
-                done = true;
+            if (numCompletedContainers.get() == numTotalContainers) {  //要申请的container任务已全部完成；
+                done = true;   //程序结束
             }
         }
 
+        /**
+         * 收到了ResourceManager 分配的 containers
+         * 然后就在每个container上启动应用；
+         */
         public void onContainersAllocated(List<Container> allocatedContainers) {
             LOG.info("Got response from RM for container ask, allocatedCnt="
                     + allocatedContainers.size());
-            numAllocatedContainers.addAndGet(allocatedContainers.size());
+            numAllocatedContainers.addAndGet(allocatedContainers.size());   //numAllocatedContainers(分配的) + size
             for (Container allocatedContainer : allocatedContainers) {
-                LOG.info("Launching shell command on a new container."
+                LOG.info("Launching shell command on a new container." //在每个container上启动应用；
                         + ", containerId=" + allocatedContainer.getId()
                         + ", containerNode="
-                        + allocatedContainer.getNodeId().getHost() + ":"
+                        + allocatedContainer.getNodeId().getHost() + ":"  //host:port
                         + allocatedContainer.getNodeId().getPort()
                         + ", containerNodeURI="
-                        + allocatedContainer.getNodeHttpAddress()
+                        + allocatedContainer.getNodeHttpAddress()  //URL
                         + ", containerResourceMemory"
-                        + allocatedContainer.getResource().getMemory());
+                        + allocatedContainer.getResource().getMemory());  //内存
 
-                LaunchContainerRunnable runnableLaunchContainer = new LaunchContainerRunnable(
-                        allocatedContainer, containerListener);
+                //子线程负责各个container的启动
+                LaunchContainerRunnable runnableLaunchContainer = new LaunchContainerRunnable(allocatedContainer, containerListener);
                 Thread launchThread = new Thread(runnableLaunchContainer);
 
                 launchThreads.add(launchThread);
@@ -467,27 +496,34 @@ public class JBossApplicationMaster {
             }
         }
 
+        //当ResourceManager因为不同步等原因希望shutdown Application Master时调用；
         public void onShutdownRequest() {
             done = true;
         }
 
+        //当NodeManager的状态(健康状态 / 是否可用)发生更新时回调，
         public void onNodesUpdated(List<NodeReport> updatedNodes) {
         }
 
+        //进度，已经完成的container / 总共需要申请的container，
         public float getProgress() {
             float progress = (float) numCompletedContainers.get()
                     / numTotalContainers;
             return progress;
         }
 
+        //发生异常时回调，异常可能是由于和ResourceManager的通信异常 / 程序自身的异常，此时建议调用 resourceManager.stop(); 方法
         public void onError(Throwable e) {
             done = true;
             resourceManager.stop();
         }
     }
 
+    //实现NMClientAsync(和NodeManager进行异步通信)的 CallbackHandler
+    //相当于和NodeManager通信后的 异步获取结果的 回调方法；
     private class NMCallbackHandler implements NMClientAsync.CallbackHandler {
 
+        //相当于一个NodeManager上有多个 container ?
         private ConcurrentMap<ContainerId, Container> containers = new ConcurrentHashMap<ContainerId, Container>();
 
         public void addContainer(ContainerId containerId, Container container) {
@@ -499,6 +535,7 @@ public class JBossApplicationMaster {
             }
         }
 
+        @Override
         public void onContainerStopped(ContainerId containerId) {
             if (LOG.isLoggable(Level.FINEST)) {
                 LOG.finest("Succeeded to stop Container " + containerId);
@@ -506,6 +543,7 @@ public class JBossApplicationMaster {
             containers.remove(containerId);
         }
 
+        @Override
         public void onContainerStatusReceived(ContainerId containerId,
                                               ContainerStatus containerStatus) {
             if (LOG.isLoggable(Level.FINEST)) {
@@ -521,7 +559,7 @@ public class JBossApplicationMaster {
             }
             Container container = containers.get(containerId);
             if (container != null) {
-                nmClientAsync.getContainerStatusAsync(containerId,
+                nmClientAsync.getContainerStatusAsync(containerId,  //container启动后，通过NodeManager客户端监听container的状态；
                         container.getNodeId());
             }
         }
@@ -548,6 +586,7 @@ public class JBossApplicationMaster {
     }
 
     /**
+     * 通过执行shell命令启动container
      * Thread to connect to the {@link ContainerManagementProtocol} and launch
      * the container that will execute the shell command.
      */
@@ -559,9 +598,9 @@ public class JBossApplicationMaster {
 
         /**
          * @param lcontainer
-         *            Allocated container
+         *            Allocated container  被分配的container
          * @param containerListener
-         *            Callback handler of the container
+         *            Callback handler of the container  被分配container的监听器
          */
         public LaunchContainerRunnable(Container lcontainer,
                                        NMCallbackHandler containerListener) {
@@ -570,7 +609,7 @@ public class JBossApplicationMaster {
         }
 
         /**
-         * Connects to CM, sets up container launch context for shell command
+         * Connects to CM(ContainerManagementProtocol), sets up container launch context for shell command
          * and eventually dispatches the container start request to the CM.
          */
         public void run() {
@@ -579,12 +618,14 @@ public class JBossApplicationMaster {
 
             LOG.info("Setting up container launch container for containerid="
                     + container.getId());
+
+            //1.构建CLC!
             ContainerLaunchContext ctx = Records
                     .newRecord(ContainerLaunchContext.class);
 
-            ctx.setEnvironment(shellEnv);
+            ctx.setEnvironment(shellEnv); //1.1 为 CLC 构建执行环境
 
-            Map<String, LocalResource> localResources = new HashMap<String, LocalResource>();
+            Map<String, LocalResource> localResources = new HashMap<String, LocalResource>(); //1.2 为 CLC 设置需要本地化的资源
 
             String applicationId = container.getId().getApplicationAttemptId()
                     .getApplicationId().toString();
@@ -593,8 +634,8 @@ public class JBossApplicationMaster {
 
                 LocalResource jbossDist = Records
                         .newRecord(LocalResource.class);
-                jbossDist.setType(LocalResourceType.ARCHIVE);
-                jbossDist.setVisibility(LocalResourceVisibility.APPLICATION);
+                jbossDist.setType(LocalResourceType.ARCHIVE); //压缩文件
+                jbossDist.setVisibility(LocalResourceVisibility.APPLICATION); //应用级别
 
                 Path jbossDistPath = new Path(new URI(
                         JBossConstants.JBOSS_DIST_PATH));
@@ -631,7 +672,7 @@ public class JBossApplicationMaster {
 
             List<String> commands = new ArrayList<String>();
 
-            String host = container.getNodeId().getHost();
+            String host = container.getNodeId().getHost(); //host地址；
 
             String containerHome = conf.get("yarn.nodemanager.local-dirs")
                     + File.separator + ContainerLocalizer.USERCACHE
@@ -695,6 +736,7 @@ public class JBossApplicationMaster {
     }
 
     /**
+     * 构建向ResourceManager发送container请求的Request
      * Setup the request that will be sent to the RM for the container ask.
      *
      * //@param numContainers
@@ -703,14 +745,13 @@ public class JBossApplicationMaster {
      */
     private ContainerRequest setupContainerAskForRM() {
         Priority pri = Records.newRecord(Priority.class);
-        pri.setPriority(requestPriority);
+        pri.setPriority(requestPriority); //优先级
 
         Resource capability = Records.newRecord(Resource.class);
-        capability.setMemory(containerMemory);
-        capability.setVirtualCores(2);
+        capability.setMemory(containerMemory); //内存
+        capability.setVirtualCores(2);       //CPU核数
 
-        ContainerRequest request = new ContainerRequest(capability, null, null,
-                pri);
+        ContainerRequest request = new ContainerRequest(capability, null, null,pri);
         LOG.info("Requested container ask: " + request.toString());
         return request;
     }
